27c565d90880f6c8b600e0d0430603f8d99dbd00
Altino Leandro Rodiguês <136701250+AltinoLeandroRodrigues@users.noreply.github.com>
2023-08-09 16:07:30 +0100

    adicionei 3 cards

---
+++ asset/js/responsividade.js
@@ -0,0 +1,10 @@
+function  clicar(){
+    let botao = document.getElementById('');
+    let Menu = document.getElementById('Menu');
+
+    if (Menu.classList.contains('active')) {
+        Menu.classList.remove('active')
+    } else {
+        Menu.classList.add('active')
+    }
+}
